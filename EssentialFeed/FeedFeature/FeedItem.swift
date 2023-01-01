b18b8f21f1723447ad998a8a6a1cebbcbd46590f
Max <k1071303@gmail.com>
2023-01-01 21:33:37 +0800

    Add missing file

---
+++ EssentialFeed/FeedFeature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Max Kai on 2023/1/1.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
