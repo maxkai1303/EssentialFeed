6a1e499157879fa1b076c2f8f62da54ff6ebd9b5
Max <k1071303@gmail.com>
2023-01-02 11:32:04 +0800

    Move `RemoteFeedLoader` components to production
    - 把 RemoteFeedLoader 拆離開測試檔案

---
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Max Kai on 2023/1/2.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
