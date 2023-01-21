9f72179bd1ef97d1069f79c5419297cbf408278d
Max <k1071303@gmail.com>
2023-01-21 17:08:16 +0800

    Simplify type declarations with a helper function

--- EssentialFeedTests/FeedAPI/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/FeedAPI/RemoteFeedLoaderTest.swift
@@ -40,7 +40,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func testLoadDeliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.connectivity)) {
+        expect(sut, toCompleteWithResult: failure(.connectivity)) {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         }
@@ -52,7 +52,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
         
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+            expect(sut, toCompleteWithResult: failure(.invalidData)) {
                 let json = makeItemJSON([])
                 client.complete(withStatusCode: code, data: json, at: index)
             }
@@ -61,7 +61,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     func testLoadDeliversErrorOn200HTTPResponsWithInvalidJSON() {
         let (sut, client) = makeSUT()
-        expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut, toCompleteWithResult: failure(.invalidData)) {
             let invalidJSON = Data("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
         }
@@ -117,6 +117,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
         trackFromMemoryLeak(sut)
         trackFromMemoryLeak(client)
         return(sut, client)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func trackFromMemoryLeak(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
